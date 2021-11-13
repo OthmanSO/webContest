1634df61cba9b455eae0469df0a8e293c6b29327
Osama Ismail <odiab200012@gmail.com>
2021-11-13 11:50:42 +0200

    home_page

---
+++ in.js
@@ -0,0 +1,14 @@
+function osama(){
+
+    var x=document.getElementById("sec");
+    // x.innerHTML="<div class='osama1'>ahmad</div>";
+    x.innerHTML+="<div class='osama1'></div>";
+
+    // x.style.backgroundColor="red";
+
+}
+function osama(){
+
+   
+    
+}
